3d8182e40b8be46d8a161e84639922e04d0df630
C3NZ <vmarcella@microsoft.com>
2021-07-27 10:30:33 -0700

    [update] lambda-sh versioning and the rest of the azure vm create command.

--- .custom/azure/vm.sh
+++ .custom/azure/vm.sh
@@ -6,13 +6,30 @@ az_vm_create() {
         --description "Name of the virtual machine" \
         --default "vmarcella-$RANDOM"
 
+    lambda_args_add \
+        --name "resource-group" \
+        --description "The resource group to deploy to" \
+        --default "vmarcella-rg"
+
+    lambda_args_add \
+        --name "username" \
+        --description "The admin username to use" \
+        --default "azlinux"
+
     lambda_args_compile "$@"
 
-    if [ $# = 1 ]; then
+    if [ $? = 1 ]; then
         return
     fi
 
-    lambda_log_info "$LAMBDA_name"
+    # Add custom sizes and other configurations
+    az vm create \
+        -g "$LAMBDA_resource_group" \
+        -n "$LAMBDA_name" \
+        --admin-username "$LAMBDA_username" \
+        --size "Standard_D2S_V3" \
+        --image "Canonical:UbuntuServer:18.04-LTS:latest"
 
+    lambda_assert_last_command_ok "Failed to create a virtual machine."
     lambda_args_cleanup
 }
