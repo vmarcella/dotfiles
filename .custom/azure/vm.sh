1e377703b8fe63b2aeb3c79f258114574b7c7c40
C3NZ <vmarcella@microsoft.com>
2021-07-29 14:52:07 -0700

    [add] a subscription function set to default within our test subscription.

--- .custom/azure/vm.sh
+++ .custom/azure/vm.sh
@@ -1,6 +1,26 @@
 source $HOME/.custom/lambda-sh/lambda.sh
 
+az_subscription_set() {
+    trap "lambda_args_cleanup" EXIT ERR SIGINT RETURN
+
+    lambda_args_add \
+        --name "name" \
+        --description "Name of the subscription to set." \
+        --default "Azure Linux Team - Dev-Test - non-ExpressRoute"
+
+    lambda_args_compile "$@"
+
+    if [ $? = 1 ]; then
+        return
+    fi
+
+    lambda_log_info "Setting account subscription to: $LAMBDA_name"
+    az account set --subscription "$LAMBDA_name"
+}
+
 az_vm_create() {
+    trap "lambda_args_cleanup" EXIT ERR SIGINT RETURN
+
     lambda_args_add \
         --name "name" \
         --description "Name of the virtual machine" \
@@ -29,7 +49,5 @@ az_vm_create() {
         --admin-username "$LAMBDA_username" \
         --size "Standard_D2S_V3" \
         --image "Canonical:UbuntuServer:18.04-LTS:latest"
-
     lambda_assert_last_command_ok "Failed to create a virtual machine."
-    lambda_args_cleanup
 }
