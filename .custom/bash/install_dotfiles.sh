9a11b49cba4a7f445eb25c6c97e5a7bc92c0f464
vmarcella <cenz@cenz.io>
2021-12-28 11:42:40 -0800

    [add] an install script for setting up this repository.

---
+++ .custom/bash/install_dotfiles.sh
@@ -0,0 +1,31 @@
+#
+# $HOME/.custom/bash/install_dotfiles.sh
+# Install's dotfiles from https://github.com/vmarcella/dotfiles
+#
+# This script comes from: https://www.atlassian.com/git/tutorials/dotfiles
+
+# ----------------------------------- FETCH ------------------------------------
+
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+
+pushd "$HOME" > /dev/null
+
+echo ".dotfiles" >> .gitignore
+
+git clone --bare --recurse-submodules \
+    https://github.com/vmarcella/dotfiles $HOME/.dotfiles
+
+# ----------------------------------- BACKUP -----------------------------------
+
+mkdir -p .dotfiles-backup
+config checkout 2>&1 | egrep "\s+\." \
+    | awk {'print $1'} \
+    | xargs -I{} mv {} .dotfiles-backup/{}
+
+# ---------------------------------- COMPLETE ----------------------------------
+
+config checkout
+config config --local status.showUntrackedFiles no
+
+
+popd > /dev/null  # $HOME
