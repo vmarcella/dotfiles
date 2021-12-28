dd974bfdbbdf8ab2c548360bb02f49ba5b2d84ce
vmarcella <cenz@cenz.io>
2021-12-28 10:18:52 -0800

    [add] centralized alias location for both bash & zsh.

---
+++ .custom/bash/aliases.sh
@@ -0,0 +1,15 @@
+# Tmux aliases
+alias tattach="tmux attach-session -t"
+alias tkill="tmux kill-session -t"
+alias tnew="tmux new -s"
+alias tdetach="tmux detach"
+
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+alias start_keychain='eval `keychain --eval --agents ssh id_ed25519 id_rsa azlinux`'
+
+alias steam='flatpak run --filesystem=/yeet com.valvesoftware.Steam'
+
+# Pats factorio server connection info.
+alias factorio_server_connect='wg-quick up /etc/wireguard/wg0.conf'
+alias factorio_server_disconnect='wg-quick down /etc/wireguard/wg0.conf'
