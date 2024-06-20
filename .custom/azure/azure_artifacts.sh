10f7eecae959e22dbaf396cb2c5a6b0cb3ec59d2
vmarcella <cenz@cenz.io>
2024-06-20 15:06:12 -0700

    [add] helper to install the ev2 cli.

--- .custom/azure/azure_artifacts.sh
+++ .custom/azure/azure_artifacts.sh
@@ -1,9 +1,9 @@
 refresh_feed_credentials() {
-	ALIAS=$(az account show --query user.name -o tsv)
-	ACCESS_TOKEN=$(az account get-access-token --query accessToken -o tsv)
+  ALIAS=$(az account show --query user.name -o tsv)
+  ACCESS_TOKEN=$(az account get-access-token --query accessToken -o tsv)
 
-	export POETRY_HTTP_BASIC_AZTUX_USERNAME="$ALIAS"
-	export POETRY_HTTP_BASIC_AZTUX_PASSWORD="$ACCESS_TOKEN"
-	export POETRY_HTTP_BASIC_AZTUX_ARTIFACTS_USERNAME="$ALIAS"
-	export POETRY_HTTP_BASIC_AZTUX_ARTIFACTS_PASSWORD="$ACCESS_TOKEN"
+  export POETRY_HTTP_BASIC_AZTUX_USERNAME="$ALIAS"
+  export POETRY_HTTP_BASIC_AZTUX_PASSWORD="$ACCESS_TOKEN"
+  export POETRY_HTTP_BASIC_AZTUX_ARTIFACTS_USERNAME="$ALIAS"
+  export POETRY_HTTP_BASIC_AZTUX_ARTIFACTS_PASSWORD="$ACCESS_TOKEN"
 }
---
+++ .custom/azure/ev2.sh
@@ -0,0 +1,29 @@
+install_ev2() {
+  echo "Fetching ev2 repository..."
+
+  mkdir -p "$HOME/dev/azure"
+
+  pushd "$HOME/dev/azure"
+
+  git clone https://msazure.visualstudio.com/DefaultCollection/One/_git/Azure-Express-Cli
+
+  pushd Azure-Express-Cli
+
+  # Update source and build the ev2 binary.
+  git pull origin master
+  go mod vendor
+  go build -o linux/ev2 ev2
+
+  # Copy the ev2 binary and configurations to my home directory
+  mkdir -p "$HOME/.ev2"
+  cp -r linux/ev2 "$HOME/.ev2"
+  cp -r ev2cli_release.yaml configurations "$HOME/.ev2"
+  sudo ln -sf "$HOME/.ev2/ev2" /usr/local/bin/ev2
+
+  popd # $HOME/dev/azureJ
+  popd # Azure-Express-Cli
+
+  ev2 tabcompletion --shell zsh >"$HOME/.zsh/completions/_ev2"
+
+  echo "ev2 and it's completions have been installed."
+}
