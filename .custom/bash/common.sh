5744840bb38cd776b270a09e07e1c6ec23df9be6
vmarcella <cenz@cenz.io>
2024-07-05 17:06:27 -0700

    [add] a custom julia-up config file.

--- .custom/bash/common.sh
+++ .custom/bash/common.sh
@@ -1,8 +1,9 @@
 source "$HOME/.custom/bash/aliases.sh"
+source "$HOME/.custom/bash/julia.sh"
 source "$HOME/.custom/azure/vm.sh"
 
 DISTRIBUTION="$(cat /etc/os-release &>/dev/null | grep ^ID= | cut -d= -f2)"
 
 if [[ "$DISTRIBUTION" == "ubuntu" ]]; then
-    source "$HOME/.custom/ubuntu/apt.sh"
+	source "$HOME/.custom/ubuntu/apt.sh"
 fi
---
+++ .custom/bash/julia.sh
@@ -0,0 +1,8 @@
+# >>> juliaup initialize >>>
+
+# !! Contents within this block are managed by juliaup !!
+
+PATH=('/Users/vmarcella/.juliaup/bin' "$PATH")
+export PATH
+
+# <<< juliaup initialize <<<
